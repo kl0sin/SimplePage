24e06e54fb7aad42a4684fd1a4125af87079be3a
Mateusz Kłosiński <mkklosin@gmail.com>
2017-11-28 00:17:48 +0100

    Gulp init

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const inject = require('gulp-inject');
+const clean = require('gulp-clean');
+const runSequence = require('run-sequence');
+const image = require('gulp-image');
+
+gulp.task('serve', ['build'], function() {
+    browserSync.init({
+        server: './dist'
+    });
+    gulp.watch('src/*.html', ['html', 'build']);
+    gulp.watch('src/styles/**/*.scss', ['sass', 'inject']);
+    gulp.watch('src/scripts/**/*.js', ['js', 'inject']);
+    gulp.watch('src/media/**/*.{png,jpeg,gif,svg}', ['image']);
+});
+
+gulp.task('build', function(callback) {
+    runSequence('clean', 'html', 'sass', 'js', 'inject', 'image', callback);
+});
+
+gulp.task('html', function() {
+    return gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('image', function() {
+    return gulp.src('./src/media/**/*.{png,jpeg,gif,svg}')
+        .pipe(image())
+        .pipe(gulp.dest('./dist/media'));
+})
+
+gulp.task('sass', function() {
+    return gulp.src('./src/styles/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/styles'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    return gulp.src('src/scripts/**/*.js')
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('inject', function() {
+    return gulp.src('./dist/*.html')
+        .pipe(inject(gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false}), {relative: true}))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('./dist/*', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('default', ['serve']);
